a34487ff0a8494d230432fe89413098aad6e7e81
bchenSyr <36112405+bchenSyr@users.noreply.github.com>
2018-05-14 16:17:15 -0400

    Create recursive-iterative-reverse-LinkedList-20180514.java

---
+++ recursive-iterative-reverse-LinkedList-20180514.java
@@ -0,0 +1,67 @@
+// Created Date: May 14, 2018
+// Application: practice reverse LinkedList recursively and iteratively
+
+package myMain;
+
+public class MainClass {
+		
+	//Time Complexity: O(n);
+	//Space Complexity: O(1);
+	public static ListNode IterativeReverse(ListNode head) {
+				
+		ListNode prev = null;
+		ListNode curr = head;
+		ListNode next = null;
+		
+		while(curr != null) {			
+			
+			next = curr.next;
+			curr.next = prev;		
+			
+			prev = curr;	
+			curr = next;						
+		}			
+		return prev;
+	}
+	
+	//Time Complexity: O(n);
+	//Space Complexity: O(1);
+	public static ListNode RecursiveReverse(ListNode head) {
+		
+		if(head == null || head.next == null) return head;
+		
+		ListNode newHead = RecursiveReverse(head.next);
+		
+		ListNode node = head.next;
+		node.next = head;
+		head.next = null;
+				
+		return newHead;
+	}
+	
+	
+	public static void printList(ListNode head) {
+		ListNode curr = head;
+		
+		while(curr != null) {
+			System.out.print(curr.value +" ");
+			curr = curr.next;
+		}		
+		System.out.print("\n");
+	}
+	
+	public static void main(String[] args) {	
+				
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		
+		// ListNode newhead  = RecursiveReverse(head);
+		
+		ListNode newhead = IterativeReverse(head);
+		printList(newhead);
+
+	}
+}
