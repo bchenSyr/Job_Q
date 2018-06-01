3b634d4c1a50813e37ba068f11b078a4075c70a5
bchenSyr <36112405+bchenSyr@users.noreply.github.com>
2018-05-31 22:49:30 -0400

    Update RecursionPower-20180514.java
    review-20180531

--- RecursionPower-20180514.java
+++ RecursionPower-20180514.java
@@ -1,5 +1,5 @@
-//Created Date: May 14, 2018
-//Application: calculate power recursively 
+// Created Date: May 14, 2018
+// Application: calculate power recursively 
 
 package myMain;
 
@@ -11,39 +11,40 @@ public class RecursionPower {
   // Space Complexity: O(1)
   public static long powerB1(int a, int b) {  	
   
-		 if(b == 0) return 1;		   
-		 if(a == 0) return 0;			  
-		 long result = 1;
+	 if(b == 0) return 1;		   
+	 if(a == 0) return 0;			  
+	 long result = 1;
 		 
-		 for(long i = 0; i < b; i++) {
-			 result *= a;
-		 }		 
-		return result;	 
-	}	
+	 for(long i = 0; i < b; i++) {
+		 result *= a;
+	 }		 
+	return result;	 
+  }	
 	
   //bad algorithm, too much recursion
-	// Time Complexity: O(b)
-	// Space Complexity: O(b)
-	public static long powerB2(int a, int b) {
+  // Time Complexity: O(b)
+  // Space Complexity: O(b)
+  public static long powerB2(int a, int b) {
+			  
+	if(b == 0) return 1;	// base case
+	if(a == 0) return 0;	// this can omit
 		
-		if(a == 0) return 0;
-		if(b == 0) return 1;
-		
-		return a * powerB2(a, b - 1);			    
-	}
+	return a * powerB2(a, b - 1);			    
+  }
 
-	// Time Complexity: O(log(b))
-	// Space Complexity: O(log(b))
-	public static long power(int a, int b) {
-		if(a == 0) return 0;
-		if(b == 0) return 1;
-		long half = power(a, b/2);
+
+  // Time Complexity: O(log(b))
+  // Space Complexity: O(log(b))
+  public static long power(int a, int b) {
+	if(a == 0) return 0;
+	if(b == 0) return 1;
+	long half = power(a, b / 2);
 		
-		if(b % 2 == 0) return half * half;
-		else return half * half * a;	
-	}	
+	if(b % 2 == 0) return half * half;
+	else return half * half * a;	
+  }	
 	  		    	 
-	public static void main(String[] args) {	
-		System.out.print(power(1,2147483647)); 
-	}
+  public static void main(String[] args) {	
+	System.out.print(power(1,2147483647)); 
+  }
 }
